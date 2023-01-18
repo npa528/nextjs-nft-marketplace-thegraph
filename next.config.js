c7597e2b1535cf8998377bf024f5da2c5bc0ce96
nikos papadakis <npapadakis@gmail.com>
2023-01-18 18:07:18 +0200

    fleek

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     trailingSlash: true,
+
+    exportPathMap: function () {
+        return {
+            "/": { page: "/" },
+        }
+    },
 }
 
 module.exports = nextConfig
