0c74ecac14e96ba5db11977facfb42e1597658cc
nikos papadakis <npapadakis@gmail.com>
2023-01-19 23:47:00 +0200

    env variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    env: {
+        NEXT_PUBLIC_APP_ID: "001",
+        NEXT_PUBLIC_SUBGRAPH_URL:
+            "https://api.studio.thegraph.com/query/40818/nft-marketplace/v0.0.1",
+    },
 }
 
 module.exports = nextConfig
